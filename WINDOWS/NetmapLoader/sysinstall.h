3d1131e8f82438ffd9fda0b3f24f8ac0931f3368
Luigi Rizzo <rizzo@iet.unipi.it>
2015-08-01 19:43:53 -0700

    explain what we need, change date to copyright

--- WINDOWS/NetmapLoader/sysinstall.h
+++ WINDOWS/NetmapLoader/sysinstall.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2013-2015 Universita` di Pisa. All rights reserved.
+* Copyright (C) 2015 Universita` di Pisa. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions
@@ -23,7 +23,7 @@
 * SUCH DAMAGE.
 */
 
-//copy from netmap.h, we don't need everything in this project
+// we only need a few entries here, not the whole netmap.h
 
 #define DRIVER_FUNC_INSTALL     0x01
 #define DRIVER_FUNC_REMOVE      0x02
