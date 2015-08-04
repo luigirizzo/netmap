db948c789cdf4353b27c8b4a94dbda970cf1a5c7
Luigi Rizzo <rizzo@iet.unipi.it>
2015-08-03 17:20:02 -0700

    more diff reduction with master

--- sys/dev/netmap/netmap_mem2.c
+++ sys/dev/netmap/netmap_mem2.c
@@ -60,6 +60,7 @@ MALLOC_DEFINE(M_NETMAP, "netmap", "Network memory map");
 
 #include <net/netmap.h>
 #include <dev/netmap/netmap_kern.h>
+#include "netmap_virt.h"
 #include "netmap_mem2.h"
 
 #ifdef _WIN32
