1775a0a1af1d7a664012225596fc14a761024278
Vincenzo Maffione <v.maffione@gmail.com>
2016-02-22 17:51:34 +0100

    ptnet: restore PTNET_IO_END original meaning

--- sys/dev/netmap/netmap_virt.h
+++ sys/dev/netmap/netmap_virt.h
@@ -232,9 +232,9 @@ struct paravirt_csb {
 #define PTNET_IO_NUM_RX_RINGS	40
 #define PTNET_IO_NUM_TX_SLOTS	44
 #define PTNET_IO_NUM_RX_SLOTS	48
-#define PTNET_IO_END		64
-#define PTNET_IO_TXKICK		PTNET_IO_END
-#define PTNET_IO_RXKICK		(PTNET_IO_END+4)
+#define PTNET_IO_END		52
+#define PTNET_IO_TXKICK		128
+#define PTNET_IO_RXKICK		132
 #define PTNET_IO_MASK           0xff
 
 /* If defined, CSB is allocated by the guest, not by the host. */
