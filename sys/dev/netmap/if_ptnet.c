f61fd0528732c9a564a808c498f3121163ec2ef8
Vincenzo Maffione <v.maffione@gmail.com>
2016-07-13 12:16:37 +0200

    freebsd: ptnet: don't use deprecated CTRL register

--- sys/dev/netmap/if_ptnet.c
+++ sys/dev/netmap/if_ptnet.c
@@ -679,10 +679,6 @@ ptnet_irqs_init(struct ptnet_softc *sc)
 		cpu_cur = CPU_NEXT(cpu_cur);
 	}
 
-	/* Tell the hypervisor that we have allocated the MSI-X vectors,
-	 * so that it can do its own setup. */
-	bus_write_4(sc->iomem, PTNET_IO_CTRL, PTNET_CTRL_IRQINIT);
-
 	return 0;
 err_path:
 	ptnet_irqs_fini(sc);
@@ -694,10 +690,6 @@ ptnet_irqs_fini(struct ptnet_softc *sc)
 {
 	device_t dev = sc->dev;
 	int i;
-
-	/* Tell the hypervisor that we are going to deallocate the
-	 * MSI-X vectors, so that it can do its own cleanup. */
-	bus_write_4(sc->iomem, PTNET_IO_CTRL, PTNET_CTRL_IRQFINI);
 
 	for (i = 0; i < sc->num_rings; i++) {
 		struct ptnet_queue *pq = sc->queues + i;
