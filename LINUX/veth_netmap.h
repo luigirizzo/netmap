4791ff5b6d8120add74376dfdb2567eecfce8bde
Vincenzo Maffione <v.maffione@gmail.com>
2016-12-21 11:30:28 +0100

    linux: veth: update copyright

--- LINUX/veth_netmap.h
+++ LINUX/veth_netmap.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Vincenzo Maffione. All rights reserved.
+ * Copyright (C) 2014-2016 Vincenzo Maffione. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
