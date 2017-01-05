edc2ca1e544cae26e416872e8b5fc7afd97f84c9
Vincenzo Maffione <v.maffione@gmail.com>
2017-01-05 09:54:13 +0100

    netmap: remove obsolete code

--- sys/dev/netmap/netmap.c
+++ sys/dev/netmap/netmap.c
@@ -2736,22 +2736,6 @@ netmap_notify(struct netmap_kring *kring, int flags)
 	return NM_IRQ_COMPLETED;
 }
 
-#if 0
-static int
-netmap_notify(struct netmap_adapter *na, u_int n_ring,
-enum txrx tx, int flags)
-{
-	if (tx == NR_TX) {
-		KeSetEvent(notes->TX_EVENT, 0, FALSE);
-	}
-	else
-	{
-		KeSetEvent(notes->RX_EVENT, 0, FALSE);
-	}
-	return 0;
-}
-#endif
-
 /* called by all routines that create netmap_adapters.
  * provide some defaults and get a reference to the
  * memory allocator
