a6cf650b2ea3297d0a76cd836fd50f7ed9ec3efd
Seth Hall <seth@corelight.com>
2017-01-12 11:47:56 -0500

    lb: Broala name change to Corelight

--- apps/lb/lb.c
+++ apps/lb/lb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Broala and Universita` di Pisa. All rights reserved.
+ * Copyright (C) 2017 Corelight, Inc. and Universita` di Pisa. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
