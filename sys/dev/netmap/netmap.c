luigirizzo/netmap

commit af213b07aaffecfb8c62addf9fec83deb6bd0dc9

linux/veth: import more reg/unreg code from pipes
The old code for veth register/unregister could not handle veth
interfaces appearing and disappearing while in use, possibily
causing several problems including leackage of netdev references
and netmap buffers.

The main problem was that veths were treated like normal hw interfaces,
and therefore their special clean-up functions where not being called
after a NETDEV_UNREGISTER.

Another problem was that the link between the two veths is severed during
unregister; as a consequence, the old code was not able to clean up the
remote side while shutting down the local side of the pipe.

sys/dev/netmap/netmap.c
@@ -2919,7 +2919,7 @@ netmap_hw_dtor(struct netmap_adapter *na)
  * Return 0 on success, ENOMEM otherwise.
  */
 int
-netmap_attach_ext(struct netmap_adapter *arg, size_t size)
+netmap_attach_ext(struct netmap_adapter *arg, size_t size, int override_reg)
 {
 	struct netmap_hw_adapter *hwna = NULL;
 	struct ifnet *ifp = NULL;
@@ -2938,8 +2938,10 @@ netmap_attach_ext(struct netmap_adapter *arg, size_t size)
 	hwna->up = *arg;
 	hwna->up.na_flags |= NAF_HOST_RINGS | NAF_NATIVE;
 	strncpy(hwna->up.name, ifp->if_xname, sizeof(hwna->up.name));
-	hwna->nm_hw_register = hwna->up.nm_register;
-	hwna->up.nm_register = netmap_hw_reg;
+	if (override_reg) {
+		hwna->nm_hw_register = hwna->up.nm_register;
+		hwna->up.nm_register = netmap_hw_reg;
+	}
 	if (netmap_attach_common(&hwna->up)) {
 		nm_os_free(hwna);
 		goto fail;

LINUX/netmap_linux.c
@@ -2244,6 +2244,8 @@ module_exit(linux_netmap_fini);
 /* export certain symbols to other modules */
 EXPORT_SYMBOL(netmap_attach);		/* driver attach routines */
 EXPORT_SYMBOL(netmap_attach_ext);
+EXPORT_SYMBOL(netmap_adapter_get);
+EXPORT_SYMBOL(netmap_adapter_put);
 #ifdef WITH_PTNETMAP_GUEST
 EXPORT_SYMBOL(netmap_pt_guest_attach);	/* ptnetmap driver attach routine */
 EXPORT_SYMBOL(netmap_pt_guest_rxsync);	/* ptnetmap generic rxsync */

LINUX/veth_netmap.h
@@ -36,6 +36,12 @@
 static int veth_open(struct ifnet *ifp);
 static int veth_close(struct ifnet *ifp);
 
+struct netmap_veth_adapter {
+	struct netmap_hw_adapter up;
+	struct netmap_veth_adapter *peer;
+	int peer_ref;
+};
+
 /* To be called under RCU read lock */
 static struct netmap_adapter *
 veth_get_peer_na(struct netmap_adapter *na)
@@ -43,13 +49,26 @@ veth_get_peer_na(struct netmap_adapter *na)
 	struct ifnet *ifp = na->ifp;
 	struct veth_priv *priv = netdev_priv(ifp);
 	struct ifnet *peer_ifp;
+	struct netmap_veth_adapter *vna =
+		(struct netmap_veth_adapter *)na;
 
-	peer_ifp = rcu_dereference(priv->peer);
-	if (!peer_ifp) {
-		return NULL;
+	if (vna->peer == NULL) {
+		peer_ifp = rcu_dereference(priv->peer);
+		if (!peer_ifp) {
+			return NULL;
+		}
+		/* cache the na pointer so that we can retrieve it
+		 * and do our clean-up even when the peer_ifp is
+		 * detached from us
+		 */
+		vna->peer = (struct netmap_veth_adapter *)NA(peer_ifp);
+		netmap_adapter_get(&vna->peer->up.up);
+		vna->peer_ref = 1;
+		/* also set the cross reference from the peer_na to us */
+		vna->peer->peer = vna;
 	}
 
-	return NA(peer_ifp);
+	return &vna->peer->up.up;
 }
 
 /*
@@ -79,6 +98,17 @@ krings_needed(struct netmap_adapter *na)
 	return false;
 }
 
+static void
+veth_netmap_dtor(struct netmap_adapter *na)
+{
+	struct netmap_veth_adapter *vna =
+		(struct netmap_veth_adapter *)na;
+	if (vna->peer_ref) {
+		vna->peer_ref = 0;
+		netmap_adapter_put(&vna->peer->up.up);
+	}
+}
+
 /*
  * Register/unregister. We are already under netmap lock.
  * This register function is similar to the one used by
@@ -90,6 +120,8 @@ krings_needed(struct netmap_adapter *na)
 static int
 veth_netmap_reg(struct netmap_adapter *na, int onoff)
 {
+	struct netmap_veth_adapter *vna =
+		(struct netmap_veth_adapter *)na;
 	struct netmap_adapter *peer_na;
 	struct ifnet *ifp = na->ifp;
 	bool was_up;
@@ -179,7 +211,17 @@ veth_netmap_reg(struct netmap_adapter *na, int onoff)
 		veth_open(ifp);
 	}
 
-	return error;
+	if (vna->peer_ref)
+		return 0;
+	if (onoff) {
+		vna->peer->peer_ref = 0;
+		netmap_adapter_put(na);
+	} else {
+		netmap_adapter_get(na);
+		vna->peer->peer_ref = 1;
+	}
+
+	return 0;
 }
 
 static int
@@ -295,8 +337,10 @@ veth_netmap_attach(struct ifnet *ifp)
 	na.nm_rxsync = netmap_pipe_rxsync;
 	na.nm_krings_create = veth_netmap_krings_create;
 	na.nm_krings_delete = veth_netmap_krings_delete;
+	na.nm_dtor = veth_netmap_dtor;
 	na.num_tx_rings = na.num_rx_rings = 1;
-	netmap_attach(&na);
+	netmap_attach_ext(&na, sizeof(struct netmap_veth_adapter),
+			0 /* do not ovveride reg */);
 }
 
 /* end of file */

LINUX/virtio_netmap.h
@@ -738,7 +738,7 @@ virtio_netmap_attach(struct virtnet_info *vi)
 	na.nm_config = virtio_netmap_config;
 	na.nm_intr = virtio_netmap_intr;
 
-	ret = netmap_attach_ext(&na, sizeof(struct netmap_virtio_adapter));
+	ret = netmap_attach_ext(&na, sizeof(struct netmap_virtio_adapter), 1);
 	if (ret) {
 		D("Failed to attach virtio-net interface");
 		return;

sys/dev/netmap/netmap_kern.h
@@ -1188,7 +1188,7 @@ static __inline void nm_kr_start(struct netmap_kring *kr)
  *	virtual ports (vale, pipes, monitor)
  */
 int netmap_attach(struct netmap_adapter *);
-int netmap_attach_ext(struct netmap_adapter *, size_t size);
+int netmap_attach_ext(struct netmap_adapter *, size_t size, int override_reg);
 void netmap_detach(struct ifnet *);
 int netmap_transmit(struct ifnet *, struct mbuf *);
 struct netmap_slot *netmap_reset(struct netmap_adapter *na,

sys/dev/netmap/netmap_pt.c
@@ -1510,7 +1510,7 @@ netmap_pt_guest_attach(struct netmap_adapter *arg, void *csb,
 	if (arg->nm_mem == NULL)
 		return ENOMEM;
 	arg->na_flags |= NAF_MEM_OWNER;
-	error = netmap_attach_ext(arg, sizeof(struct netmap_pt_guest_adapter));
+	error = netmap_attach_ext(arg, sizeof(struct netmap_pt_guest_adapter), 1);
 	if (error)
 		return error;
 

commit e7d670a9c727bec5802fbec3ac09a9e7bd727af8

fix compilation error introduced by af213b07

sys/dev/netmap/netmap.c
@@ -2989,7 +2989,8 @@ fail:
 int
 netmap_attach(struct netmap_adapter *arg)
 {
-	return netmap_attach_ext(arg, sizeof(struct netmap_hw_adapter));
+	return netmap_attach_ext(arg, sizeof(struct netmap_hw_adapter),
+			1 /* override nm_reg */);
 }
 
 