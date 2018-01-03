6cfdaf6b0352ffd8c56b646562fb36e21b7bfd20
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2018-01-04 00:15:52 +0100

    extmem: always use kmap

--- sys/dev/netmap/netmap_mem2.c
+++ sys/dev/netmap/netmap_mem2.c
@@ -2255,7 +2255,7 @@ netmap_mem_ext_create(struct nmreq *nmr, int *perror)
 			}
 			off = noff;
 			if (nr_pages > 0)
-				clust = page_to_virt(*pages);
+				clust = kmap(*pages);
 		}
 		p->objtotal = j;
 		p->numclusters = p->objtotal;
