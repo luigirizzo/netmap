0b4cdcb9bf7324a8f48f7b4d52a4d02cf0f2bed0
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2018-01-04 00:15:33 +0100

    extmem: fix access out of bounds

--- sys/dev/netmap/netmap_mem2.c
+++ sys/dev/netmap/netmap_mem2.c
@@ -2239,7 +2239,8 @@ netmap_mem_ext_create(struct nmreq *nmr, int *perror)
 				skip = PAGE_SIZE;
 			}
 			off = noff;
-			clust = page_to_virt(*pages);
+			if (nr_pages > 0)
+				clust = page_to_virt(*pages);
 		}
 		p->objtotal = j;
 		p->numclusters = p->objtotal;
