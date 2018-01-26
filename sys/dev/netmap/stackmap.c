59fb7d2b1dadcb90e0074de572f8e3479c0ee45f
Michio Honda <micchie@sfc.wide.ad.jp>
2018-01-26 14:40:27 +0100

    stack: don't forward invalid scb
    XXX do better, this occurs because something is wrong

--- sys/dev/netmap/stackmap.c
+++ sys/dev/netmap/stackmap.c
@@ -423,7 +423,15 @@ stmp_bdg_flush(struct netmap_kring *kring)
 				tmp.buf_idx = next;
 				scb = NMCB_BUF(NMB(na, &tmp));
 				next = scb->next;
+				if (unlikely(!stmp_cb_valid(scb))) {
+					D("invalid scb %p next %u", scb, next);
+					goto skip;
+				}
 				ts = scb_slot(scb);
+				if (unlikely(ts == NULL)) {
+					D("null ts %p next %u", ts, next);
+					goto skip;
+				}
 				if (stmp_cb_rstate(scb) == SCB_M_TXREF) {
 					nonfree[nonfree_num++] = j;
 				}
@@ -435,6 +443,7 @@ stmp_bdg_flush(struct netmap_kring *kring)
 				ts->fd = 0;
 				ts->flags |= NS_BUF_CHANGED;
 				rs->flags |= NS_BUF_CHANGED;
+skip:
 				j = nm_next(j, lim_rx);
 				sent++;
 			} while (next != STACKMAP_FT_NULL && --howmany);
