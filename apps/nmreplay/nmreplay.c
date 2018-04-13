5b8ad405a594aae351bbfe393793c9d4dbb5d2af
Vincenzo Maffione <v.maffione@gmail.com>
2018-04-13 17:33:21 +0200

    nmreplay: use proper C comments

--- apps/nmreplay/nmreplay.c
+++ apps/nmreplay/nmreplay.c
@@ -24,71 +24,68 @@
  */
 
 
-#if 0 /* COMMENT */
-
-This program implements NMREPLAY, a program to replay a pcap file
-enforcing the output rate and possibly random losses and delay
-distributions.
-It is meant to be run from the command line and implemented with a main
-control thread for monitoring, plus a thread to push packets out.
-
-The control thread parses command line arguments, prepares a
-schedule for transmission in a memory buffer and then sits
-in a loop where it periodically reads traffic statistics from
-the other threads and prints them out on the console.
-
-The transmit buffer contains headers and packets. Each header
-includes a timestamp that determines when the packet should be sent out.
-A "consumer" thread cons() reads from the queue and transmits packets
-on the output netmap port when their time has come.
-
-The program does CPU pinning and sets the scheduler and priority
-for the "cons" threads. Externally one should do the
-assignment of other threads (e.g. interrupt handlers) and
-make sure that network interfaces are configured properly.
-
---- Main functions of the program ---
-within each function, q is used as a pointer to the queue holding
-packets and parameters.
-
-pcap_prod()
-
-    reads from the pcap file and prepares packets to transmit.
-    After reading a packet from the pcap file, the following information
-    are extracted which can be used to determine the schedule:
-
-    	q->cur_pkt	points to the buffer containing the packet
-	q->cur_len	packet length, excluding CRC
-	q->cur_caplen	available packet length (may be shorter than cur_len)
-	q->cur_tt	transmission time for the packet, computed from the trace.
-
-    The following functions are then called in sequence:
-
-    q->c_loss (set with the -L command line option) decides
-    	whether the packet should be dropped before even queuing.
-	This is generally useful to emulate random loss.
-	The function is supposed to set q->c_drop = 1 if the
-	packet should be dropped, or leave it to 0 otherwise.
-
-    q->c_bw (set with the -B command line option) is used to
-        enforce the transmit bandwidth. The function must store
-	in q->cur_tt the transmission time (in nanoseconds) of
-	the packet, which is typically proportional to the length
-	of the packet, i.e. q->cur_tt = q->cur_len / <bandwidth>
-	Variants are possible, eg. to account for constant framing
-	bits as on the ethernet, or variable channel acquisition times,
-	etc.
-	This mechanism can also be used to simulate variable queueing
-	delay e.g. due to the presence of cross traffic.
-
-    q->c_delay (set with the -D option) implements delay emulation.
-	The function should set q->cur_delay to the additional
-	delay the packet is subject to. The framework will take care of
-	computing the actual exit time of a packet so that there is no
-	reordering.
-
-
-#endif /* COMMENT */
+/*
+ * This program implements NMREPLAY, a program to replay a pcap file
+ * enforcing the output rate and possibly random losses and delay
+ * distributions.
+ * It is meant to be run from the command line and implemented with a main
+ * control thread for monitoring, plus a thread to push packets out.
+ *
+ * The control thread parses command line arguments, prepares a
+ * schedule for transmission in a memory buffer and then sits
+ * in a loop where it periodically reads traffic statistics from
+ * the other threads and prints them out on the console.
+ *
+ * The transmit buffer contains headers and packets. Each header
+ * includes a timestamp that determines when the packet should be sent out.
+ * A "consumer" thread cons() reads from the queue and transmits packets
+ * on the output netmap port when their time has come.
+ *
+ * The program does CPU pinning and sets the scheduler and priority
+ * for the "cons" threads. Externally one should do the
+ * assignment of other threads (e.g. interrupt handlers) and
+ * make sure that network interfaces are configured properly.
+ *
+ * --- Main functions of the program ---
+ * within each function, q is used as a pointer to the queue holding
+ * packets and parameters.
+ *
+ * pcap_prod()
+ *
+ *	reads from the pcap file and prepares packets to transmit.
+ *	After reading a packet from the pcap file, the following information
+ *	are extracted which can be used to determine the schedule:
+ *
+ *   	q->cur_pkt	points to the buffer containing the packet
+ *	q->cur_len	packet length, excluding CRC
+ *	q->cur_caplen	available packet length (may be shorter than cur_len)
+ *	q->cur_tt	transmission time for the packet, computed from the trace.
+ *
+ *  The following functions are then called in sequence:
+ *
+ *  q->c_loss (set with the -L command line option) decides
+ *	whether the packet should be dropped before even queuing.
+ *	This is generally useful to emulate random loss.
+ *	The function is supposed to set q->c_drop = 1 if the
+ *	packet should be dropped, or leave it to 0 otherwise.
+ *
+ *   q->c_bw (set with the -B command line option) is used to
+ *      enforce the transmit bandwidth. The function must store
+ *	in q->cur_tt the transmission time (in nanoseconds) of
+ *	the packet, which is typically proportional to the length
+ *	of the packet, i.e. q->cur_tt = q->cur_len / <bandwidth>
+ *	Variants are possible, eg. to account for constant framing
+ *	bits as on the ethernet, or variable channel acquisition times,
+ *	etc.
+ *	This mechanism can also be used to simulate variable queueing
+ *	delay e.g. due to the presence of cross traffic.
+ *
+ *   q->c_delay (set with the -D option) implements delay emulation.
+ *	The function should set q->cur_delay to the additional
+ *	delay the packet is subject to. The framework will take care of
+ *	computing the actual exit time of a packet so that there is no
+ *	reordering.
+ */
 
 // debugging macros
 #define NED(_fmt, ...)	do {} while (0)
@@ -115,21 +112,20 @@ pcap_prod()
 
 /*
  *
-A packet in the queue is q_pkt plus the payload.
-
-For the packet descriptor we need the following:
-
-    -	position of next packet in the queue (can go backwards).
-	We can reduce to 32 bits if we consider alignments,
-	or we just store the length to be added to the current
-	value and assume 0 as a special index.
-    -	actual packet length (16 bits may be ok)
-    -	queue output time, in nanoseconds (64 bits)
-    -	delay line output time, in nanoseconds
-	One of the two can be packed to a 32bit value
-
-A convenient coding uses 32 bytes per packet.
-
+ * A packet in the queue is q_pkt plus the payload.
+ *
+ * For the packet descriptor we need the following:
+ *
+ *  -	position of next packet in the queue (can go backwards).
+ *	We can reduce to 32 bits if we consider alignments,
+ *	or we just store the length to be added to the current
+ *	value and assume 0 as a special index.
+ *  -	actual packet length (16 bits may be ok)
+ *  -	queue output time, in nanoseconds (64 bits)
+ *  -	delay line output time, in nanoseconds
+ *	One of the two can be packed to a 32bit value
+ *
+ * A convenient coding uses 32 bytes per packet.
  */
 
 struct q_pkt {
