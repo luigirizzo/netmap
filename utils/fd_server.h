44ac0db4a9cb831c4583445c0f1ab602c07489e8
Stefano Duo <duostefano93@gmail.com>
2018-06-18 20:20:10 +0200

    code formatted using clang-format

--- utils/fd_server.h
+++ utils/fd_server.h
@@ -1,45 +1,31 @@
 #ifndef FD_LIB_H
 #define FD_LIB_H
 
-
-
-#include <sys/socket.h>
-#include <stdint.h>
 #include <errno.h>
 #include <net/if.h>
+#include <stdint.h>
+#include <sys/socket.h>
 
 #include <net/netmap.h>
 #define NETMAP_WITH_LIBS
 #include <net/netmap_user.h>
 
-
-
 struct fd_request {
-#define FD_GET		1
-#define FD_RELEASE	2
-#define FD_CLOSE	3
-#define FD_STOP		4
+#define FD_GET 1
+#define FD_RELEASE 2
+#define FD_CLOSE 3
+#define FD_STOP 4
 	uint8_t action;
 	char if_name[IFNAMSIZ];
 };
-
-
 
 struct fd_response {
 	int32_t result;
 	struct nmreq req;
 };
 
+int send_fd(int socket, int fd, void *buf, size_t buf_size);
 
-
-int
-send_fd(int socket, int fd, void *buf, size_t buf_size);
-
-
-
-int
-recv_fd(int socket, int *fd, void *buf, size_t buf_size);
-
-
+int recv_fd(int socket, int *fd, void *buf, size_t buf_size);
 
 #endif /* FD_LIB_H */
