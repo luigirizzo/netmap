fb3287e8b1f157433ab5fa1dc5d4ca266a5c712e
Vincenzo Maffione <v.maffione@gmail.com>
2018-09-22 23:21:59 +0200

    netmap_ioctl: implement NETMAP_REQ_SYNC_KLOOP_STOP

--- sys/dev/netmap/netmap.c
+++ sys/dev/netmap/netmap.c
@@ -2636,7 +2636,17 @@ netmap_ioctl(struct netmap_priv_d *priv, u_long cmd, caddr_t data,
 		}
 
 		case NETMAP_REQ_SYNC_KLOOP_STOP: {
-			error = ENOSYS;
+			NMG_LOCK();
+			switch (priv->np_kloop_state) {
+			case NM_SYNC_KLOOP_NONE:
+				error = ENXIO;
+				break;
+			case NM_SYNC_KLOOP_ACTIVE:
+			case NM_SYNC_KLOOP_STOPPING:
+				priv->np_kloop_state = NM_SYNC_KLOOP_STOPPING;
+				break;
+			}
+			NMG_UNLOCK();
 			break;
 		}
 
