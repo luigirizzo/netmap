bcc6f3dfc9e452c040a429f63fc3db22161d3bb4
Vincenzo Maffione <v.maffione@gmail.com>
2018-09-22 11:30:17 +0200

    introduce netmap_sync_kloop() function

--- sys/dev/netmap/netmap.c
+++ sys/dev/netmap/netmap.c
@@ -2628,7 +2628,9 @@ netmap_ioctl(struct netmap_priv_d *priv, u_long cmd, caddr_t data,
 		}
 
 		case NETMAP_REQ_SYNC_KLOOP_START: {
-			error = ENOSYS;
+			struct nmreq_sync_kloop_start *req =
+				(struct nmreq_sync_kloop_start *)(uintptr_t)hdr->nr_body;
+			error = netmap_sync_kloop(req);
 			break;
 		}
 
@@ -3899,6 +3901,11 @@ nm_clear_native_flags(struct netmap_adapter *na)
 	na->na_flags &= ~NAF_NETMAP_ON;
 }
 
+int
+netmap_sync_kloop(struct nmreq_sync_kloop_start *req)
+{
+	return ENOSYS;
+}
 
 /*
  * Module loader and unloader
