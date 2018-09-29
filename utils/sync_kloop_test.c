31ca2703d43f77f4440df2d80a807249c59eeb08
Vincenzo Maffione <v.maffione@gmail.com>
2018-09-29 18:06:17 +0200

    utils: sync_kloop_test: move application processing to the main thread

--- utils/sync_kloop_test.c
+++ utils/sync_kloop_test.c
@@ -24,6 +24,9 @@ sigint_handler(int signum)
 struct context {
 	struct nm_desc *nmd;
 	const char *func;
+	struct nm_csb_atok *atok_base;
+	struct nm_csb_ktoa *ktoa_base;
+	size_t num_entries;
 };
 
 static void *
@@ -32,88 +35,24 @@ kloop_worker(void *opaque)
 	struct context *ctx = opaque;
 	struct nm_desc *nmd = ctx->nmd;
 	struct nmreq_sync_kloop_start req;
-	struct nm_csb_atok *atok_base;
-	struct nm_csb_ktoa *ktoa_base;
 	struct nmreq_header hdr;
-	size_t num_entries;
-	size_t csb_size;
-	void *csb;
 	int ret;
 
-	num_entries = nmd->last_tx_ring - nmd->first_tx_ring + 1 +
-	              nmd->last_rx_ring - nmd->first_rx_ring + 1;
-	printf("Number of CSB entries = %d\n", (int)num_entries);
-	csb_size = (sizeof(struct nm_csb_atok) + sizeof(struct nm_csb_ktoa)) *
-	           num_entries;
-	assert(csb_size > 0);
-	ret = posix_memalign(&csb, sizeof(struct nm_csb_atok), csb_size);
-	if (ret) {
-		printf("Failed to allocate CSB memory\n");
-		return NULL;
-	}
-	memset(csb, 0, csb_size);
-
+	/* The ioctl() returns on failure or when some other thread
+	 * stops the kernel loop. */
 	memset(&hdr, 0, sizeof(hdr));
 	hdr.nr_version = NETMAP_API;
 	hdr.nr_reqtype = NETMAP_REQ_SYNC_KLOOP_START;
 	hdr.nr_body    = (uintptr_t)&req;
 	hdr.nr_options = (uintptr_t)NULL;
 	memset(&req, 0, sizeof(req));
-	req.csb_atok = (uintptr_t)csb;
-	req.csb_ktoa =
-	        (uintptr_t)(csb + sizeof(struct nm_csb_atok) * num_entries);
-	ret = ioctl(nmd->fd, NIOCCTRL, &hdr);
+	req.csb_atok = (uintptr_t)ctx->atok_base;
+	req.csb_ktoa = (uintptr_t)ctx->ktoa_base;
+	ret          = ioctl(nmd->fd, NIOCCTRL, &hdr);
 	if (ret) {
 		perror("ioctl(/dev/netmap, NIOCCTRL, SYNC_KLOOP_START)");
+		exit(EXIT_FAILURE);
 	}
-
-	atok_base = (struct nm_csb_atok *)csb;
-	ktoa_base = (struct nm_csb_ktoa *)(atok_base + num_entries);
-
-	if (!strcmp(ctx->func, "tx")) {
-		while (!ACCESS_ONCE(stop)) {
-			uint16_t r;
-
-			for (r = nmd->first_tx_ring; r <= nmd->last_tx_ring;
-			     r++) {
-				struct netmap_ring *ring =
-				        NETMAP_TXRING(nmd->nifp, r);
-				struct nm_csb_atok *atok = atok_base + r;
-				struct nm_csb_ktoa *ktoa = ktoa_base + r;
-				struct netmap_slot *slot;
-				uint32_t hwtail, head;
-
-				head   = atok->head;
-				hwtail = ACCESS_ONCE(ktoa->hwtail);
-
-				if (head == hwtail) {
-					continue;
-				}
-
-				slot        = ring->slot + head;
-				slot->len   = 60;
-				slot->flags = 0;
-				{
-					char *buf =
-					        NETMAP_BUF(ring, slot->buf_idx);
-					memset(buf, 0xFF, 6);
-					memset(buf + 6, 0, 6);
-					buf[12] = 0x08;
-					buf[13] = 0x00;
-					memset(buf + 14, 'x', slot->len - 14);
-				}
-				ACCESS_ONCE(atok->head) =
-				        ACCESS_ONCE(atok->cur) =
-				                nm_ring_next(ring, head);
-				printf("ring #%u, head %u, hwtail %u\n",
-				       (unsigned int)r, (unsigned int)head,
-				       (unsigned int)hwtail);
-			}
-			usleep(1000000);
-		}
-	}
-
-	free(csb);
 
 	return NULL;
 }
@@ -132,7 +71,9 @@ int
 main(int argc, char **argv)
 {
 	const char *ifname = NULL;
+	void *csb          = NULL;
 	struct context ctx;
+	struct nm_desc *nmd;
 	pthread_t th;
 	int opt;
 	int ret;
@@ -183,20 +124,93 @@ main(int argc, char **argv)
 		return -1;
 	}
 
-	printf("ifname %s\n", ifname);
-	ctx.nmd = nm_open(ifname, NULL, 0, NULL);
-	if (!ctx.nmd) {
+	/* Open the netmap port. */
+	ctx.nmd = nmd = nm_open(ifname, NULL, 0, NULL);
+	if (!nmd) {
 		printf("nm_open(%s) failed\n", ifname);
 		return -1;
 	}
 
+	/* Allocate CSB entries. */
+	{
+		size_t csb_size;
+
+		ctx.num_entries = nmd->last_tx_ring - nmd->first_tx_ring + 1 +
+		                  nmd->last_rx_ring - nmd->first_rx_ring + 1;
+		printf("Number of CSB entries = %d\n", (int)ctx.num_entries);
+		csb_size = (sizeof(struct nm_csb_atok) +
+		            sizeof(struct nm_csb_ktoa)) *
+		           ctx.num_entries;
+		assert(csb_size > 0);
+		ret = posix_memalign(&csb, sizeof(struct nm_csb_atok),
+		                     csb_size);
+		if (ret) {
+			printf("Failed to allocate CSB memory\n");
+			return -1;
+		}
+		memset(csb, 0, csb_size);
+
+		ctx.atok_base = (struct nm_csb_atok *)csb;
+		ctx.ktoa_base =
+		        (struct nm_csb_ktoa *)(ctx.atok_base + ctx.num_entries);
+	}
+
+	/* Start the kernel worker thread. */
 	ret = pthread_create(&th, NULL, kloop_worker, &ctx);
 	if (ret) {
 		printf("pthread_create() failed: %s\n", strerror(ret));
-		nm_close(ctx.nmd);
+		nm_close(nmd);
 		return -1;
 	}
 
+	/* Run the application loop. */
+	if (!strcmp(ctx.func, "tx")) {
+		while (!ACCESS_ONCE(stop)) {
+			uint16_t r;
+
+			for (r = nmd->first_tx_ring; r <= nmd->last_tx_ring;
+			     r++) {
+				struct netmap_ring *ring =
+				        NETMAP_TXRING(nmd->nifp, r);
+				struct nm_csb_atok *atok = ctx.atok_base + r;
+				struct nm_csb_ktoa *ktoa = ctx.ktoa_base + r;
+				struct netmap_slot *slot;
+				uint32_t head;
+
+				head = atok->head;
+				/* For convenience we reuse the netmap_ring
+				 * header to store hwtail and hwcur, since the
+				 * cur, head and tail fields are not used. */
+				nm_sync_kloop_appl_read(ktoa, /*hwtail=*/&ring->tail,
+							/*hwcur=*/&ring->cur);
+
+				if (head == ring->tail) {
+					continue;
+				}
+
+				slot        = ring->slot + head;
+				slot->len   = 60;
+				slot->flags = 0;
+				{
+					char *buf =
+					        NETMAP_BUF(ring, slot->buf_idx);
+					memset(buf, 0xFF, 6);
+					memset(buf + 6, 0, 6);
+					buf[12] = 0x08;
+					buf[13] = 0x00;
+					memset(buf + 14, 'x', slot->len - 14);
+				}
+				head = nm_ring_next(ring, head);
+				nm_sync_kloop_appl_write(atok, head, head);
+				printf("ring #%u, hwcur %u, head %u, hwtail "
+				       "%u\n",
+				       (unsigned int)r, ring->cur, head, ring->tail);
+			}
+			usleep(1000000);
+		}
+	}
+
+	/* Stop the kernel worker thread. */
 	{
 		struct nmreq_header hdr;
 		int ret;
@@ -204,18 +218,21 @@ main(int argc, char **argv)
 		memset(&hdr, 0, sizeof(hdr));
 		hdr.nr_version = NETMAP_API;
 		hdr.nr_reqtype = NETMAP_REQ_SYNC_KLOOP_STOP;
-		ret            = ioctl(ctx.nmd->fd, NIOCCTRL, &hdr);
+		ret            = ioctl(nmd->fd, NIOCCTRL, &hdr);
 		if (ret) {
 			perror("ioctl(/dev/netmap, NIOCCTRL, SYNC_KLOOP_STOP)");
 		}
 	}
 
+	/* Release the allocated resources. */
 	ret = pthread_join(th, NULL);
 	if (ret) {
 		printf("pthread_join() failed: %s\n", strerror(ret));
 	}
 
-	nm_close(ctx.nmd);
+	free(csb);
+
+	nm_close(nmd);
 
 	return 0;
 }
