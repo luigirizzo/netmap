47e34727e3c8e1c0c579f3dd29bf81b479de65b9
Vincenzo Maffione <v.maffione@gmail.com>
2018-10-07 17:10:04 +0200

    linux: virtio_net.c: compatibility support for byteorder and types

--- LINUX/if_virtio_net_netmap.h
+++ LINUX/if_virtio_net_netmap.h
@@ -27,6 +27,10 @@
 #include <net/netmap.h>
 #include <netmap/netmap_kern.h>
 
+/*************************************************************************/
+/* COMPATIBILITY LAYER                                                   */
+/*************************************************************************/
+
 #if !defined(NETMAP_LINUX_VIRTIO_NET_HDR_FROM_SKB_5ARGS) && !defined(NETMAP_LINUX_VIRTIO_NET_HDR_FROM_SKB_4ARGS) && !defined(NETMAP_LINUX_VIRTIO_NET_HDR_FROM_SKB_3ARGS)
 static inline int virtio_net_hdr_to_skb(struct sk_buff *skb,
 					const struct virtio_net_hdr *hdr,
@@ -140,7 +144,81 @@ static inline int ethtool_validate_duplex(__u8 duplex)
 }
 #endif  /* NETMAP_LINUX_HAVE_ETHTOOL_VALIDATE */
 
-struct virtnet_info;
+#ifndef NETMAP_LINUX_HAVE_VIRTIO_BYTEORDER
+#include <linux/types.h>
+
+/*
+ * __virtio{16,32,64} have the following meaning:
+ * - __u{16,32,64} for virtio devices in legacy mode, accessed in native endian
+ * - __le{16,32,64} for standard-compliant virtio devices
+ */
+
+typedef __u16 __bitwise__ __virtio16;
+typedef __u32 __bitwise__ __virtio32;
+typedef __u64 __bitwise__ __virtio64;
+
+static inline bool virtio_legacy_is_little_endian(void)
+{
+#ifdef __LITTLE_ENDIAN
+	return true;
+#else
+	return false;
+#endif
+}
+
+static inline u16 __virtio16_to_cpu(bool little_endian, __virtio16 val)
+{
+	if (little_endian)
+		return le16_to_cpu((__force __le16)val);
+	else
+		return be16_to_cpu((__force __be16)val);
+}
+
+static inline __virtio16 __cpu_to_virtio16(bool little_endian, u16 val)
+{
+	if (little_endian)
+		return (__force __virtio16)cpu_to_le16(val);
+	else
+		return (__force __virtio16)cpu_to_be16(val);
+}
+
+static inline u32 __virtio32_to_cpu(bool little_endian, __virtio32 val)
+{
+	if (little_endian)
+		return le32_to_cpu((__force __le32)val);
+	else
+		return be32_to_cpu((__force __be32)val);
+}
+
+static inline __virtio32 __cpu_to_virtio32(bool little_endian, u32 val)
+{
+	if (little_endian)
+		return (__force __virtio32)cpu_to_le32(val);
+	else
+		return (__force __virtio32)cpu_to_be32(val);
+}
+
+static inline u64 __virtio64_to_cpu(bool little_endian, __virtio64 val)
+{
+	if (little_endian)
+		return le64_to_cpu((__force __le64)val);
+	else
+		return be64_to_cpu((__force __be64)val);
+}
+
+static inline __virtio64 __cpu_to_virtio64(bool little_endian, u64 val)
+{
+	if (little_endian)
+		return (__force __virtio64)cpu_to_le64(val);
+	else
+		return (__force __virtio64)cpu_to_be64(val);
+}
+#endif  /* NETMAP_LINUX_HAVE_VIRTIO_BYTEORDER */
+
+
+/*************************************************************************/
+/* NETMAP SUPPORT                                                        */
+/*************************************************************************/
 
 static void
 virtio_net_netmap_attach(struct virtnet_info *vi)
