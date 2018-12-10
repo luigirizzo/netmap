accf1241f12e1ef97534bb6c228da35507db004d
Vincenzo Maffione <v.maffione@gmail.com>
2018-12-10 10:46:44 +0100

    utils: ctrl-api-test: use POSIX semaphores to avoid usleep()

--- utils/ctrl-api-test.c
+++ utils/ctrl-api-test.c
@@ -14,6 +14,7 @@
 #include <pthread.h>
 #include <assert.h>
 #include <time.h>
+#include <semaphore.h>
 
 #ifdef __linux__
 #include <sys/eventfd.h>
@@ -50,6 +51,8 @@ struct TestContext {
 	uint32_t nr_num_polling_cpus; /* vale polling */
 	void *csb;                    /* CSB entries (atok and ktoa) */
 	struct nmreq_option *nr_opt;  /* list of options */
+
+	sem_t *sem;	/* for thread synchronization */
 
 	struct nmport_d *nmport;      /* nmport descriptor from libnetmap */
 };
@@ -1120,6 +1123,10 @@ sync_kloop_worker(void *opaque)
 		perror("ioctl(/dev/netmap, NIOCCTRL, SYNC_KLOOP_START)");
 	}
 
+	if (ctx->sem) {
+		sem_post(ctx->sem);
+	}
+
 	pthread_exit(ret ? (void *)THRET_FAILURE : (void *)THRET_SUCCESS);
 }
 
@@ -1317,6 +1324,9 @@ sync_kloop_conflict(struct TestContext *ctx)
 	struct nmreq_opt_csb opt;
 	pthread_t th1, th2;
 	void *thret1 = THRET_FAILURE, *thret2 = THRET_FAILURE;
+	struct timespec to;
+	sem_t sem;
+	int err = 0;
 	int ret;
 
 	ret = push_csb_option(ctx, &opt);
@@ -1330,35 +1340,50 @@ sync_kloop_conflict(struct TestContext *ctx)
 	}
 	clear_options(ctx);
 
+	sem_init(&sem, 0, 0);
+	ctx->sem = &sem;
+
 	ret = pthread_create(&th1, NULL, sync_kloop_worker, ctx);
+	err |= ret;
 	if (ret) {
 		printf("pthread_create(kloop1): %s\n", strerror(ret));
-		return -1;
 	}
 
 	ret = pthread_create(&th2, NULL, sync_kloop_worker, ctx);
+	err |= ret;
 	if (ret) {
 		printf("pthread_create(kloop2): %s\n", strerror(ret));
-		return -1;
 	}
 
-	/* Try to avoid a race condition where th1 starts the loop and stops,
+	/* Wait for one of the two threads to fail to start the kloop, to
+	 * avoid a race condition where th1 starts the loop and stops,
 	 * and after that th2 starts the loop successfully. */
-	usleep(500000);
-
-	ret = sync_kloop_stop(ctx);
+	clock_gettime(CLOCK_REALTIME, &to);
+	to.tv_sec += 2;
+	ret = sem_timedwait(&sem, &to);
+	err |= ret;
 	if (ret) {
-		return ret;
+		printf("sem_timedwait() failed: %s\n", strerror(errno));
 	}
+
+	err |= sync_kloop_stop(ctx);
 
 	ret = pthread_join(th1, &thret1);
+	err |= ret;
 	if (ret) {
 		printf("pthread_join(kloop1): %s\n", strerror(ret));
 	}
 
 	ret = pthread_join(th2, &thret2);
+	err |= ret;
 	if (ret) {
 		printf("pthread_join(kloop2): %s %d\n", strerror(ret), ret);
+	}
+
+	sem_destroy(&sem);
+	ctx->sem = NULL;
+	if (err) {
+		return err;
 	}
 
 	/* Check that one of the two failed, while the other one succeeded. */
@@ -1526,6 +1551,7 @@ context_cleanup(struct TestContext *ctx)
 	}
 
 	close(ctx->fd);
+	ctx->fd = -1;
 }
 
 static int
