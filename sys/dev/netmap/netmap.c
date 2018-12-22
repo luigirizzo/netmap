luigirizzo/netmap

commit 968a6287512c701e145823e368aca5eff3e821c1

make sure RXSYNC handlers are run if there is work to do (e.g. buffers to release). This reduces chances of loss due to late wakeup of processes.

sys/dev/netmap/netmap.c
@@ -2429,6 +2429,32 @@ netmap_poll(struct netmap_priv_d *priv, int events, NM_SELRECORD_T *sr)
 	 * slots available. If this fails, then lock and call the sync
 	 * routines.
 	 */
+#if 1 /* new code- call rx if any of the ring needs to release or read buffers */
+	if (want_tx) {
+		t = NR_TX;
+		for (i = priv->np_qfirst[t]; want[t] && i < priv->np_qlast[t]; i++) {
+			kring = &NMR(na, t)[i];
+			/* XXX compare ring->cur and kring->tail */
+			if (!nm_ring_empty(kring->ring)) {
+				revents |= want[t];
+				want[t] = 0;	/* also breaks the loop */
+			}
+		}
+	}
+	if (want_rx) {
+		want_rx = 0; /* look for a reason to run the handlers */
+		t = NR_RX;
+		for (i = priv->np_qfirst[t]; i < priv->np_qlast[t]; i++) {
+			kring = &NMR(na, t)[i];
+			if (kring->ring->cur == kring->ring->tail /* try fetch new buffers */
+			    || kring->rhead != kring->ring->head /* release buffers */) {
+				want_rx = 1;
+			}
+		}
+		if (!want_rx)
+			revents |= want[t]; /* we have data */
+	}
+#else /* old code */
 	for_rx_tx(t) {
 		for (i = priv->np_qfirst[t]; want[t] && i < priv->np_qlast[t]; i++) {
 			kring = &NMR(na, t)[i];
@@ -2439,6 +2465,7 @@ netmap_poll(struct netmap_priv_d *priv, int events, NM_SELRECORD_T *sr)
 			}
 		}
 	}
+#endif /* old code */
 
 	/*
 	 * If we want to push packets out (priv->np_txpoll) or

commit ffdfc14b984631cbc8cbc15c97f42cbc6b583427

freebsd: fix bug in netmap_poll() optimization
This bug was introduced by 968a6287512c701e1, and it only affects
select(), which uses POLLRDNORM/POLLWRNORM. The poll() syscall is
not affected by this bug, because POLLIN is used and POLLIN == 1.

Sponsored-by: Sunny Valley Networks

sys/dev/netmap/netmap.c
@@ -3304,16 +3304,19 @@ netmap_poll(struct netmap_priv_d *priv, int events, NM_SELRECORD_T *sr)
 	}
 	if (want_rx) {
 		enum txrx t = NR_RX;
-		want_rx = 0; /* look for a reason to run the handlers */
+		int rxsync_needed = 0;
+
+		/* look for a reason to run the handlers */
 		for (i = priv->np_qfirst[t]; i < priv->np_qlast[t]; i++) {
 			kring = NMR(na, t)[i];
 			if (kring->ring->cur == kring->ring->tail /* try fetch new buffers */
 			    || kring->rhead != kring->ring->head /* release buffers */) {
-				want_rx = 1;
+				rxsync_needed = 1;
+				break;
 			}
 		}
-		if (!want_rx)
-			revents |= events & (POLLIN | POLLRDNORM); /* we have data */
+		if (!rxsync_needed)
+			revents |= want_rx; /* we have data */
 	}
 #endif
 