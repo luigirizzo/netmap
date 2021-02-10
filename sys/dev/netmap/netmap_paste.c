f1977653b5eb38f18a627a269c9eca117573d455
Michio Honda <michio.honda@ed.ac.uk>
2021-02-10 01:29:49 +0000

    Revert "freebsd: remove unnecessary sockbuf lock"
    This reverts commit 5ba3883f57631a6d9803aea3c8aa24282bf694ea.

--- sys/dev/netmap/netmap_paste.c
+++ sys/dev/netmap/netmap_paste.c
@@ -1149,10 +1149,11 @@ pst_unregister_socket(struct pst_so_adapter *soa, int locked)
 	} else {
 		sna->so_adapters[soa->fd] = NULL;
 		NM_SOCK_LOCK(so);
-		/* socket is dying, no need to lock sockbuf */
+		SOCKBUF_LOCK(&so->so_rcv);
 		RESTORE_SOUPCALL(so, soa);
 		RESTORE_SODTOR(so, soa);
 		pst_wso(NULL, so);
+		SOCKBUF_UNLOCK(&so->so_rcv);
 		NM_SOCK_UNLOCK(so);
 	}
 	nm_os_free(soa);
