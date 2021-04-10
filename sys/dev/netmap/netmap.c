luigirizzo/netmap

commit cecc4c06d8cc1cb60cf7e0b7d46cfa29cdf361d5

offsets: fix minor issues

sys/dev/netmap/netmap.c
@@ -2334,7 +2334,7 @@ netmap_offsets_init(struct netmap_priv_d *priv, struct nmreq_header *hdr)
 			if ((kring->offset_mask & mask) != mask ||
 			     kring->offset_max < max_offset) {
 				if (netmap_verbose)
-					nm_prinf("%s: cannot decrease"
+					nm_prinf("%s: cannot increase"
 						 "offset mask and/or max"
 						 "(current: mask=%llx,max=%llu",
 							kring->name,
@@ -2408,7 +2408,7 @@ netmap_compute_buf_len(struct netmap_priv_d *priv)
 			maxframe = mtu + ETH_HLEN +
 				ETH_FCS_LEN + VLAN_HLEN;
 			if (maxframe < target) {
-				target = kring->offset_gap;
+				target = maxframe;
 			}
 		}
 
@@ -3248,6 +3248,7 @@ nmreq_opt_size_by_type(uint32_t nro_reqtype, uint64_t nro_size)
 		break;
 	case NETMAP_REQ_OPT_OFFSETS:
 		rv = sizeof(struct nmreq_opt_offsets);
+		break;
 	}
 	/* subtract the common header */
 	return rv - sizeof(struct nmreq_option);

sys/dev/netmap/netmap_kern.h
@@ -828,11 +828,11 @@ struct netmap_adapter {
 	 *      for RX rings, where we want to disallow writes outside of the
 	 *      netmap buffer. The l value must be computed taking into account
 	 *      the stipulated max_offset (o), possibily increased if there are
-	 *      alignemnt constrains, the maxframe (m), if known, and the
+	 *      alignment constraints, the maxframe (m), if known, and the
 	 *      current NETMAP_BUF_SIZE (b) of the memory region used by the
 	 *      adapter. We want the largest supported l such that o + l <= b.
 	 *      If m is known to be <= b - o, the callback may also choose the
-	 *      largest l <= b, ignoring the offset.  The buf_align field is
+	 *      largest l <= m, ignoring the offset.  The buf_align field is
 	 *      most important for TX rings when there are offsets.  The user
 	 *      will see this value in the ring->buf_align field.  Misaligned
 	 *      offsets will cause the corresponding packets to be silently

commit bf116f8e35b4d914afe14ba8d7ee3e89ee6e2ab3

use safer defaults for hwbuf_len and partially revert cecc4c06d8
We must make sure that incoming packets will never overflow the netmap
buffers, even when the user is using the offset feature. In the typical
scenario, the netmap buffer is 2KiB and, with an MTU of 1500, there are
~500 bytes available for user offsets.

Unfortunately, some NICs accept incoming packets even when they are
larger then the MTU. This means that the only way to stop DMA from
overflowing the netmap buffers, when offsets are allowed, is to choose
an hardware buffer lenght which is smaller than the netmap buffer
lenght. For most NICs and for 2KiB netmap buffers, this means 1024
bytes, which is unconveniently small.

The current code, due to a regression introduced in commit cecc4c06d8,
will select the small hardware buf size even when offsets are not
in use. The main purpose of this patch is to fix this bug by returning
to the normal behavior for the no-offsets case.

At the same time, the patch pushes the handling of the offset case
to the lower level driver code, so that it can be made NIC-specific
(in future patches).

sys/dev/netmap/netmap.c
@@ -2401,6 +2401,12 @@ out:
 
 }
 
+
+/* set the hardware buffer length in each one of the newly opened rings
+ * (hwbuf_len field in the kring struct). The purpose it to select
+ * the maximum supported input buffer lenght that will not cause writes
+ * outside of the available space, even when offsets are in use.
+ */
 static int
 netmap_compute_buf_len(struct netmap_priv_d *priv)
 {
@@ -2410,32 +2416,44 @@ netmap_compute_buf_len(struct netmap_priv_d *priv)
 	int error = 0;
 	unsigned mtu = 0;
 	struct netmap_adapter *na = priv->np_na;
-	uint64_t target, maxframe;
-
-	if (na->ifp != NULL)
-		mtu = nm_os_ifnet_mtu(na->ifp);
+	uint64_t target;
 
 	foreach_selected_ring(priv, t, i, kring) {
-
+		/* rings that are already active have their hwbuf_len
+		 * already set and we cannot change it.
+		 */
 		if (kring->users > 1)
 			continue;
 
+		/* For netmap buffers which are not shared among several ring
+		 * slots (the normal case), the available space is the buf size
+		 * minus the max offset declared by the user at open time.  If
+		 * the user plans to have several slots pointing to different
+		 * offsets into the same large buffer, she must also declare a
+		 * "minium gap" between two such consecutive offsets. In this
+		 * case the user-declared 'offset_gap' is taken as the
+		 * available space and offset_max is ignored.
+		 */
+
+		/* start with the normal case (unshared buffers) */
 		target = NETMAP_BUF_SIZE(kring->na) -
 			kring->offset_max;
+		/* if offset_gap is zero, the user does not intend to use
+		 * shared buffers. In this case the minimum gap between
+		 * two consective offsets into the same buffer can be
+		 * assumed to be equal to the buffer size. In this way
+		 * offset_gap always contains the available space ignoring
+		 * offset_max. This may be used by drivers of NICs that
+		 * are guaranteed to never write more than MTU bytes, even
+		 * if the input buffer is larger: if the MTU is less
+		 * than the target they can set hwbuf_len to offset_gap.
+		 */
 		if (!kring->offset_gap)
 			kring->offset_gap =
 				NETMAP_BUF_SIZE(kring->na);
+
 		if (kring->offset_gap < target)
 			target = kring->offset_gap;
-
-		if (mtu) {
-			maxframe = mtu + ETH_HLEN +
-				ETH_FCS_LEN + VLAN_HLEN;
-			if (maxframe < target) {
-				target = maxframe;
-			}
-		}
-
 		error = kring->nm_bufcfg(kring, target);
 		if (error)
 			goto out;