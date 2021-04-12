a4f289592e16b6c9435705fe6d478dca0be8dbf7
Michio Honda <michio.honda@ed.ac.uk>
2021-04-12 21:08:52 +0100

    paste: kernel files

--- sys/dev/netmap/netmap_bdg.h
+++ sys/dev/netmap/netmap_bdg.h
@@ -181,6 +181,7 @@ int netmap_bwrap_reg(struct netmap_adapter *, int onoff);
 int netmap_bdg_detach_locked(struct nmreq_header *hdr, void *auth_token);
 int netmap_vp_reg(struct netmap_adapter *na, int onoff);
 int netmap_vp_rxsync(struct netmap_kring *kring, int flags);
+int netmap_vp_rxsync_locked(struct netmap_kring *kring, int flags);
 int netmap_bwrap_intr_notify(struct netmap_kring *kring, int flags);
 int netmap_bwrap_notify(struct netmap_kring *kring, int flags);
 int netmap_bwrap_attach_common(struct netmap_adapter *na,
