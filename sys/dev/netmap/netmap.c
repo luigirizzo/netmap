luigirizzo/netmap

commit 335cb5aee93b9ba7b1353fead20082ecb85befcc

netmap_update_config: update na->name to cope with reconfigurations

sys/dev/netmap/netmap.c
@@ -763,6 +763,10 @@ netmap_update_config(struct netmap_adapter *na)
 {
 	struct nm_config_info info;
 
+	if (na->ifp) {
+		strlcpy(na->name, na->ifp->if_xname, sizeof(na->name));
+	}
+
 	bzero(&info, sizeof(info));
 	if (na->nm_config == NULL ||
 	    na->nm_config(na, &info)) {

commit f9d73acbb93762a693e339ed515723cbe11c57e3

bwrap: only update hw adapters names
This fixes a regression introduced by commit 335cb5aee9, whereby
the name of the software adapter 'valeXYZ:...' may inadvertently
lose the vale prefix, making it impossibile to detach it from vale.

Fix: 882

sys/dev/netmap/netmap.c
@@ -763,7 +763,7 @@ netmap_update_config(struct netmap_adapter *na)
 {
 	struct nm_config_info info;
 
-	if (na->ifp) {
+	if (na->ifp && !nm_is_bwrap(na)) {
 		strlcpy(na->name, na->ifp->if_xname, sizeof(na->name));
 	}
 

sys/dev/netmap/netmap_bdg.h
@@ -192,7 +192,6 @@ void netmap_uninit_bridges2(struct nm_bridge *, u_int);
 int netmap_bdg_update_private_data(const char *name, bdg_update_private_data_fn_t callback,
 	void *callback_data, void *auth_token);
 int netmap_bdg_config(struct nm_ifreq *nifr);
-int nm_is_bwrap(struct netmap_adapter *);
 
 #define NM_NEED_BWRAP (-2)
 #endif /* _NET_NETMAP_BDG_H_ */

sys/dev/netmap/netmap_kern.h
@@ -1155,6 +1155,7 @@ struct netmap_bwrap_adapter {
 	struct netmap_vp_adapter *saved_na_vp;
 	int (*nm_intr_notify)(struct netmap_kring *kring, int flags);
 };
+int nm_is_bwrap(struct netmap_adapter *na);
 int nm_bdg_polling(struct nmreq_header *hdr);
 
 int netmap_bdg_attach(struct nmreq_header *hdr, void *auth_token);