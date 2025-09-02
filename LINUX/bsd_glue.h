luigirizzo/netmap

commit bc3a1263c3e1776458f5ec0255051e31e58feda9

linux/build: check for hrtimer_setup

LINUX/bsd_glue.h
@@ -78,6 +78,15 @@
 #define HRTIMER_MODE_REL	HRTIMER_REL
 #endif
 
+#ifdef NETMAP_LINUX_HAVE_HRTIMER_SETUP
+#define nm_hrtimer_setup	hrtimer_setup
+#else
+#define nm_hrtimer_setup(t_, f_, c_, m_) do {	\
+	hrtimer_init(t_, c_, m_);		\
+	(t_)->function = (c_);			\
+} while (0)
+#endif
+
 #ifndef NETMAP_LINUX_HAVE_SKB_COPY_LINEAR
 #define skb_copy_from_linear_data_offset(skb, offset, to, copy)	\
 	memcpy(to, (skb)->data + offset, copy)

LINUX/configure
@@ -2008,6 +2008,18 @@ EOF
 	}
 EOF
 
+  # check for hrtimer_setup
+  add_test 'have HRTIMER_SETUP' <<EOF
+	#include <linux/hrtimer.h>
+
+	void dummy(struct hrtimer *timer,
+		enum hrtimer_restart (*function)(struct hrtimer *),
+		clockid_t clock_id, enum hrtimer_mode mode)
+	{
+		hrtimer_setup(timer, function, clock_id, mode);
+	}
+EOF
+
   #####################################################
   # checks related to drivers                         #
   #####################################################

LINUX/netmap_linux.c
@@ -510,8 +510,8 @@ void
 nm_os_mitigation_init(struct nm_generic_mit *mit, int idx,
 			struct netmap_adapter *na)
 {
-	hrtimer_init(&mit->mit_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-	mit->mit_timer.function = &generic_timer_handler;
+	nm_hrtimer_setup(&mit->mit_timer, &generic_timer_handler,
+			CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	mit->mit_pending = 0;
 	mit->mit_ring_idx = idx;
 	mit->mit_na = na;

commit 88440dc2a59b45419998cfb37c6461fdd43bedf0

linux: fix regression introduced by bc3a1263c3

LINUX/bsd_glue.h
@@ -83,7 +83,7 @@
 #else
 #define nm_hrtimer_setup(t_, f_, c_, m_) do {	\
 	hrtimer_init(t_, c_, m_);		\
-	(t_)->function = (c_);			\
+	(t_)->function = (f_);			\
 } while (0)
 #endif
 